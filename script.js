27aab275eaea9f867a58f54673f63ba69cd7d34b
fmakximiano <fernasbae@gmail.com>
2022-06-26 15:12:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+//1 .Criar fnção que exiba  mensagem console
+function mensagem(){
+    console.log("Estou no Vai na Web")
+}
+//2. Criar função que receba  o meu nome como(parametro) e exiba no consolee)
+function nome(nome){
+    console.log ("Meu nome é:")
+}
+nome ("Fernanda")
+//3. Criar função que receba nome e estilo de musical preferido(parametro) e exiba no consolo
+
+function assunto ("nome","idade","estilomusical")
+{
+    console.log(nome,idade,estilomusical)
+}
+assunto("Fernanda","26 anos","K-pop")
+//4. Criar fução que receba um filme e múscica(parametro) e exiba no console
+function sobre (filme,musica){
+    console.log(filme,musica)
+}
+sobre ("Ação","MagicShop")
+
+//5. Criar uma função que retorno o tripo do número recebido parametro no parametro da função
+function numero (numero1){
+    return numero1 * 3
+}
+console.log ("Multiplicado o valor por 3 é igual:", numero (26))
